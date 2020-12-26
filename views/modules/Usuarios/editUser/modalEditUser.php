f46929847268cb3da26cc877d32ce52f4ec90e7a
Alexis García <alexisfvgarcia@gmail.com>
2020-12-26 15:54:06 -0600

    Correccion formulario
    Se corrigio el error de modal-lg y formulario para el modulo de usuarios

--- views/modules/Usuarios/editUser/modalEditUser.php
+++ views/modules/Usuarios/editUser/modalEditUser.php
@@ -1,24 +1,23 @@
 <!-- The Modal -->
 <div class="modal fade" id="modalEditarUsuario" data-backdrop="static">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-        <form role="form" id="formEditarUsuario" method="post" enctype="multipart/form-data"
-            <div class="modal-content">
+    <form role="form" class="modal-dialog modal-dialog-centered modal-lg" id="formEditarUsuario" method="post" enctype="multipart/form-data">
+        <div class="modal-content">
 
-                <!-- Modal Header -->
-                <div class="modal-header" style="border-top: #6610F2 12px solid;">
-                    <h1 class="modal-title">Editar datos del usuario</h1>
-                    <button type="button" class="close btn text-black closeModalEditUser" data-dismiss="modal">×</button>
-                </div>
+            <!-- Modal Header -->
+            <div class="modal-header" style="border-top: #6610F2 12px solid;">
+                <h1 class="modal-title">Editar datos del usuario</h1>
+                <button type="button" class="close btn text-black closeModalEditUser" data-dismiss="modal">×</button>
+            </div>
 
-                <!-- Modal body -->
-                <div class="modal-body" style="border-top: #6610F2 1px solid;">
-                    <?php
-                    include "editUser.php";
-                    // Llamar metodo para ingresar categoria a la BD
-                    $respuesta = ControladorUsuarios::ctrActualizarUsuario();                    
+            <!-- Modal body -->
+            <div class="modal-body" style="border-top: #6610F2 1px solid;">
+                <?php
+                include "editUser.php";
+                // Llamar metodo para ingresar categoria a la BD
+                $respuesta = ControladorUsuarios::ctrActualizarUsuario();
 
-                    if ($respuesta == "ok") {
-                        echo '<script>
+                if ($respuesta == "ok") {
+                    echo '<script>
                                 swal({
                                     title: "Actualización exitosa!",
                                     text: "El usuario se actualizó correctamente.",
@@ -27,8 +26,8 @@
                                     window.location = "mainUsuarios";
                                 });
                              </script>';
-                    } else if ($respuesta == "error") {
-                        echo '<script>
+                } else if ($respuesta == "error") {
+                    echo '<script>
                                 swal({
                                     title: "Error!",
                                     text: "Ha ocurrido un error con la conexión a la base de datos.",
@@ -37,16 +36,15 @@
                                     window.location = "mainUsuarios";
                                 });
                               </script>';
-                    }
-                    ?>
-                </div>
-
-                <!-- Modal footer -->
-                <div class="modal-footer" style="border-top: #6610F2 1px solid;">
-                    <button type="button" class="btn btn-danger closeModalEditUser" data-dismiss="modal">Cancelar</button>
-                    <button type="submit" class="btn btn-primary">Guardar</button>
-                </div>
+                }
+                ?>
             </div>
-        </form>
-    </div>
+
+            <!-- Modal footer -->
+            <div class="modal-footer" style="border-top: #6610F2 1px solid;">
+                <button type="button" class="btn btn-danger closeModalEditUser" data-dismiss="modal">Cancelar</button>
+                <button type="submit" class="btn btn-primary">Guardar cambios</button>
+            </div>
+        </div>
+    </form>
 </div>
