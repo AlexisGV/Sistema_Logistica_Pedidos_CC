279cfe0b63e0e322b92d6426d8a4b7b749341a83
Alexis García <alexisfvgarcia@gmail.com>
2021-01-13 10:53:36 -0600

    Modal para ver detalle del pedido completo
    Se creo el modal para ver los detalles del pedido

--- views/modules/Pedidos/modalVerPedido.php
+++ views/modules/Pedidos/modalVerPedido.php
@@ -37,6 +37,37 @@ MODAL - VER DETALLE PEDIDO
                         <div class="text-center text-xl-right">Teléfono: 5542189135</div>
                     </div>
                 </div>
+
+                <div class="mb-3">
+                    <h5 class="font-weight-bold text-center">Detalles del pedido</h5>
+                    <div class="contenedorProductosModal bg-light border-bottom border-secondary">
+                        <div class="productoNuevo row py-3 border-top border-secondary">
+                            <div class="col-12 col-xl-10 pb-2 pb-xl-0"><span class="d-block font-weight-bold text-center d-xl-none">Descripción del producto:</span>Prueba 1 | Marca: Bacardi | Forma: Cilindrica | Corte(s): Horizontal; Vertical | Otro corte: No | Acabado(s): Pulido | Otro acabado: No | Observación: Sin observaciones</div>
+                            <div class="col-4 col-xl-1 text-left text-xl-center"><span class="d-inline-block d-xl-none font-weight-bold mr-1">Cantidad:</span>1</div>
+                            <div class="col-6 col-xl-1 text-right text-xl-center"><span class="d-inline-block d-xl-none font-weight-bold mr-1">Precio:</span>$ 1,474.00</div>
+                        </div>
+                        <div class="productoNuevo row py-3 border-top border-secondary">
+                            <div class="col-12 col-xl-10 pb-2 pb-xl-0"><span class="d-block font-weight-bold text-center d-xl-none">Descripción del producto:</span>Prueba 2 | Marca: Bacardi | Forma: Cilindrica | Corte(s): Horizontal; Vertical | Otro corte: No | Acabado(s): Pulido | Otro acabado: No | Observación: Sin observaciones</div>
+                            <div class="col-4 col-xl-1 text-left text-xl-center"><span class="d-inline-block d-xl-none font-weight-bold mr-1">Cantidad:</span>1</div>
+                            <div class="col-6 col-xl-1 text-right text-xl-center"><span class="d-inline-block d-xl-none font-weight-bold mr-1">Precio:</span>$ 1,474.00</div>
+                        </div>
+                        <div class="productoNuevo row py-3 border-top border-secondary">
+                            <div class="col-12 col-xl-10 pb-2 pb-xl-0"><span class="d-block font-weight-bold text-center d-xl-none">Descripción del producto:</span>Prueba 3 | Marca: Bacardi | Forma: Cilindrica | Corte(s): Horizontal; Vertical | Otro corte: No | Acabado(s): Pulido | Otro acabado: No | Observación: Sin observaciones</div>
+                            <div class="col-4 col-xl-1 text-left text-xl-center"><span class="d-inline-block d-xl-none font-weight-bold mr-1">Cantidad:</span>1</div>
+                            <div class="col-6 col-xl-1 text-right text-xl-center"><span class="d-inline-block d-xl-none font-weight-bold mr-1">Precio:</span>$ 1,474.00</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-3">
+                    <div class="d-flex flex-column flex-xl-row justify-content-between">
+                        <div id="viewSubtotal" class="text-right"><span class="font-weight-bold mr-1">Subtotal:</span>$ 1,450.00</div>
+                        <div id="viewIVA" class="text-right"><span class="font-weight-bold mr-1">IVA Aplicado:</span>16 %</div>
+                        <div id="viewTotal" class="text-right"><span class="font-weight-bold mr-1">Total:</span>$ 1,450.00</div>
+                        <div id="viewAnticipo" class="text-right mt-2 mt-xl-0"><span class="font-weight-bold mr-1">Anticipo:</span>$ 1,400.00</div>
+                        <div id="viewDeuda" class="text-right"><span class="font-weight-bold mr-1">Debe:</span>$ 40.00</div>
+                    </div>
+                </div>
             </div>
             <!-- FIN CUERPO DEL MODAL
                 -------------------------------------------------- -->
@@ -44,8 +75,7 @@ MODAL - VER DETALLE PEDIDO
             <!-- PIE DEL MODAL
                 -------------------------------------------------- -->
             <div class="modal-footer" style="border-top: #28a745 1px solid;">
-                <button type="button" class="btn btn-danger closeModalVerDetallePedido" data-dismiss="modal">Cancelar</button>
-                <button type="submit" class="btn btn-primary">Guardar cambios</button>
+                <button type="button" class="btn btn-danger closeModalVerDetallePedido" data-dismiss="modal">Cerrar ventana</button>
             </div>
             <!-- FIN PIE DEL MODAL
                 -------------------------------------------------- -->
