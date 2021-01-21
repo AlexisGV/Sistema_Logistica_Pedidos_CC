59aad45b6856a39ddaff48e08aef8efe9ec00e49
Alexis García <alexisfvgarcia@gmail.com>
2021-01-21 09:40:29 -0600

    Correcion en tabla de administrar pedidos
    Se realizo una corrección al mostrar los pedidos del modulo de administrar ventas. Se agregaron validaciones para mostrar el estatus

--- views/modules/Pedidos/tablaPedidos.php
+++ views/modules/Pedidos/tablaPedidos.php
@@ -1,5 +1,5 @@
 <?php
-setlocale(LC_ALL,"spanish.utf8");
+setlocale(LC_ALL, "spanish.utf8");
 date_default_timezone_set('UTC');
 date_default_timezone_set("America/Mexico_City");
 $fechaActual = date('Y-m-d');
@@ -21,8 +21,20 @@ $fechaActual = date('Y-m-d');
         <?php
         $tabla = "pedido";
         $item = "Fecha_Compromiso";
-        $marcas = ControladorPedidos::ctrTraerRegistrosDescendentes($tabla, $item);
-        foreach ($marcas as $key => $value) :
+        $pedidos = ControladorPedidos::ctrTraerRegistrosDescendentes($tabla, $item);
+
+        foreach ($pedidos as $key => $value) :
+
+            $fechaEntrega = $value["Fecha_Entrega"];
+
+            $tabla = "actualizaciones_pedido";
+            $idPedido = $value["Id_Pedido"];
+            $item = "Orden";
+            $estados = ControladorPedidos::ctrTraerEstadoPedido($tabla, $idPedido, $item);
+
+            $orden = $estados[0]["Orden"];
+            $estado = $estados[0]["Nombre_Estatus"];
+
         ?>
             <tr>
                 <td scope="row" style="width: 3px; max-width: 8px;"><?php echo $key + 1; ?></td>
@@ -31,54 +43,45 @@ $fechaActual = date('Y-m-d');
                 <td><?php echo strftime("%A, %d de %B del %Y a las %r", strtotime($value["Fecha_Compromiso"])); ?></td>
                 <td>
                     <?php
+                    if ($fechaEntrega != null && $fechaEntrega != "") :
+
+                        echo '<span class="badge bg-danger" style="font-size:1rem;">Entregado</span>';
+
+                    else :
+
                         $date1 = new DateTime($fechaActual);
                         $date2 = new DateTime($value["Fecha_Compromiso"]);
                         $diff = $date1->diff($date2);
                         $dias = $diff->days;
 
-                        if ( $dias > 14 ) {
-                            echo '<span class="badge bg-success" style="font-size:1rem;">'. $dias . ' días</span>';
-                        }else if ( $dias <= 14 && $dias > 7 ) {
-                            echo '<span class="badge bg-info" style="font-size:1rem;">'. $dias . ' días</span>';
-                        }else if ( $dias <= 7 && $dias > 3 ) {
-                            echo '<span class="badge bg-warning" style="font-size:1rem;">'. $dias . ' días</span>';
-                        }else{
-                            echo '<span class="badge bg-danger" style="font-size:1rem;">'. $dias . ' días</span>';
+                        if ($dias > 14) {
+                            echo '<span class="badge bg-success" style="font-size:1rem;">' . $dias . ' días</span>';
+                        } else if ($dias <= 14 && $dias > 7) {
+                            echo '<span class="badge bg-info" style="font-size:1rem;">' . $dias . ' días</span>';
+                        } else if ($dias <= 7 && $dias > 3) {
+                            echo '<span class="badge bg-warning" style="font-size:1rem;">' . $dias . ' días</span>';
+                        } else {
+                            echo '<span class="badge bg-danger" style="font-size:1rem;">' . $dias . ' días</span>';
                         }
+
+                    endif;
                     ?>
                 </td>
                 <td>
                     <?php
-                        $tabla = "actualizaciones_pedido";
-                        $idPedido = $value["Id_Pedido"];
-                        $item = "Orden";
-                        $estados = ControladorPedidos::ctrTraerEstadoPedido($tabla, $idPedido, $item);
-                        foreach ($estados as $key => $value) :
-
-                            $orden = $value["Orden"];
-                            $estado = $value["Nombre_Estatus"];
-
-                            if ( $orden == 1 ) :
-                                echo '<span class="badge bg-primary" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 2 ):
-                                echo '<span class="badge bg-success" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 3 ):
-                                echo '<span class="badge bg-danger" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 4 ):
-                                echo '<span class="badge bg-indigo" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 5 ):
-                                echo '<span class="badge bg-navy" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 6 ):
-                                echo '<span class="badge bg-danger" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 7 ):
-                                echo '<span class="badge bg-success" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 8 ):
-                                echo '<span class="badge bg-primary" style="font-size:1rem;">'. $estado . '</span>';
-                            elseif ( $orden == 9 ):
-                                echo '<span class="badge bg-maroon" style="font-size:1rem;">'. $estado . '</span>';
-                            endif;
-
-                        endforeach;
+                    if ($orden == 1 || $orden == 8) :
+                        echo '<span class="badge bg-primary" style="font-size:1rem;">' . $estado . '</span>';
+                    elseif ($orden == 2 || $orden == 7) :
+                        echo '<span class="badge bg-success" style="font-size:1rem;">' . $estado . '</span>';
+                    elseif ($orden == 3 || $orden == 6) :
+                        echo '<span class="badge bg-danger" style="font-size:1rem;">' . $estado . '</span>';
+                    elseif ($orden == 4) :
+                        echo '<span class="badge bg-indigo" style="font-size:1rem;">' . $estado . '</span>';
+                    elseif ($orden == 5) :
+                        echo '<span class="badge bg-navy" style="font-size:1rem;">' . $estado . '</span>';
+                    elseif ($orden == 9) :
+                        echo '<span class="badge bg-maroon" style="font-size:1rem;">' . $estado . '</span>';
+                    endif;
                     ?>
                 </td>
                 <td>
