92549f797433645a31891fb4a39b3378833e6038
Alexis García <alexisfvgarcia@gmail.com>
2021-02-03 11:23:28 -0600

    Validación permisos en modulo Levantar Pedido

--- views/pages/levantarPedido.php
+++ views/pages/levantarPedido.php
@@ -29,6 +29,14 @@ CONTENEDOR
     <section class="content">
         <div class="container-fluid">
 
+            <!--=============================================
+            OBTENER PERMISOS PARA ADMINISTRAR PEDIDOS
+            =============================================-->
+            <?php
+            $modulo = "Administración de pedidos";
+            $permisosPedidos = ControladorPermisos::ctrObtenerPermisos($modulo);
+            ?>
+
             <div class="row">
                 <div class="col">
                     <div class="card">
@@ -36,43 +44,68 @@ CONTENEDOR
                             <h3 class="card-title">Orden de pedido</h3>
                         </div>
 
-                        <form method="POST" id="formAddPedido">
-                            <div class="card-body">
-                                <?php include "views/modules/Pedidos/addPedido/addPedido.php"; ?>
+                        <?php
 
-                                <!--=============================================
-                                TITULO DETALLES DEL PEDIDO
-                                =============================================-->
-                                <h5 class="card-subtitle mt-lg-1 mb-3">Detalles del pedido</h5>
+                        if (intval($permisosPedidos["C"]) == 1) :
 
-                                <!--=============================================
-                                BORDE DE LOS DETALLES DEL PRODUCTO
-                                =============================================-->
-                                <div class="border">
+                        ?>
+
+                            <form method="POST" id="formAddPedido">
+                                <div class="card-body">
+                                    <?php include "views/modules/Pedidos/addPedido/addPedido.php"; ?>
+
                                     <!--=============================================
-                                    CONTENEDOR DE PRODUCTOS QUE CONFORMAN EL PEDIDO
+                                    TITULO DETALLES DEL PEDIDO
                                     =============================================-->
-                                    <div id="contenedorProductosKit" class="pt-3 px-2"></div>
-                                    <!--============  FIN DE CONTENEDOR DE PRODUCTOS QUE CONFORMAN EL PEDIDO  =============-->
+                                    <h5 class="card-subtitle mt-lg-1 mb-3">Detalles del pedido</h5>
 
-                                    <!-- BOTON PARA AGREGAR NUEVOS PRODUCTOS -->
-                                    <div class="col-12 text-center mb-3">
-                                        <input type="hidden" name="informacionPedido" id="informacionPedido">
-                                        <input type="hidden" name="listaProductos" id="listaProductos">
-                                        <button type="button" class="btn btn-outline-success btnAgregarProducto" data-toggle="modal" data-target="#modalAddProducto"><i class="fas fa-plus-circle mr-1"></i> Agregar producto</button>
+                                    <!--=============================================
+                                    BORDE DE LOS DETALLES DEL PRODUCTO
+                                    =============================================-->
+                                    <div class="border">
+                                        <!--=============================================
+                                        CONTENEDOR DE PRODUCTOS QUE CONFORMAN EL PEDIDO
+                                        =============================================-->
+                                        <div id="contenedorProductosKit" class="pt-3 px-2"></div>
+                                        <!--============  FIN DE CONTENEDOR DE PRODUCTOS QUE CONFORMAN EL PEDIDO  =============-->
+
+                                        <!-- BOTON PARA AGREGAR NUEVOS PRODUCTOS -->
+                                        <div class="col-12 text-center mb-3">
+                                            <input type="hidden" name="informacionPedido" id="informacionPedido">
+                                            <input type="hidden" name="listaProductos" id="listaProductos">
+                                            <button type="button" class="btn btn-outline-success btnAgregarProducto" data-toggle="modal" data-target="#modalAddProducto"><i class="fas fa-plus-circle mr-1"></i> Agregar producto</button>
+                                        </div>
+                                    </div>
+
+                                    <?php include "views/modules/Pedidos/addPedido/addTotalesPedido.php"; ?>
+
+                                </div>
+
+                                <div class="card-footer bg-transparent" style="border-top: 1px solid #1A6890;">
+                                    <div class="col-12 text-right">
+                                        <button type="submit" class="btn btn-primary">Levantar pedido</button>
                                     </div>
                                 </div>
+                            </form>
 
-                                <?php include "views/modules/Pedidos/addPedido/addTotalesPedido.php"; ?>
+                        <?php
+                            /*=============================================
+                            LLAMAMIENTO DEL MODAL PARA AGREGAR PRODUCTO
+                            =============================================*/
+                            include "views/modules/Pedidos/addPedido/addProducto/modalAddProducto.php";
 
-                            </div>
+                            /*=============================================
+                            CONTROLADOR PARA LEVANTAR EL PEDIDO
+                            =============================================*/
+                            $levantarPedido = new ControladorPedidos();
+                            $levantarPedido->ctrLevantarPedido();
 
-                            <div class="card-footer bg-transparent" style="border-top: 1px solid #1A6890;">
-                                <div class="col-12 text-right">
-                                    <button type="submit" class="btn btn-primary">Levantar pedido</button>
-                                </div>
-                            </div>
-                        </form>
+                        else :
+
+                            include "views/pages/permisosDenegados.php";
+
+                        endif;
+                        ?>
                     </div>
                 </div>
             </div>
@@ -82,17 +115,5 @@ CONTENEDOR
     <!-- FIN DE CONTENEDOR PRINCIPAL
     -------------------------------------------------- -->
 
-    <!--=============================================
-    LLAMAMIENTO DEL MODAL PARA AGREGAR PRODUCTO
-    =============================================-->
-    <?php include "views/modules/Pedidos/addPedido/addProducto/modalAddProducto.php"; ?>
-
 </div>
 <!--============  FIN DE CONTENEDOR  =============-->
-
-<?php
-
-    $levantarPedido = new ControladorPedidos();
-    $levantarPedido->ctrLevantarPedido();
-
-?>
